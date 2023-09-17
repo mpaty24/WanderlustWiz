f760b686b7c8676c3fe62cf693f4dba8230e8a2e
mpaty24 <>
2023-09-17 20:51:52 +0000

    Adding first part JavaScript to start game and hide quiz instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('Connected!');
+const $startQuizButton = document.querySelector(".buttonplay");
+const $questionsArea = document.querySelector(".questions-area");
+const $quizRules = document.querySelector(".quiz_rules");
+
+$startQuizButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    $quizRules.classList.add("hide");
+    $startQuizButton.classList.add("hide");
+    $questionsArea.classList.remove("hide");
+    displaynextQuestion();
+}
