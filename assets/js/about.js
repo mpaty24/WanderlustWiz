907e686a0377f93b4cf248102c861c4b1a82d784
mpaty24 <>
2023-09-26 21:28:32 +0000

    identation

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,54 +1,54 @@
-const WanderlustWizMembers = [
-    {
-        name: "Marcia P Fritzen",
-        position: "CEO",
-        bio: "Hello, I'm Marcia Patricia Fritzen, a dedicated Code Institute student and the founder of Wanderlust Wiz Quiz. I'm on a journey to combine my passion for coding with my love for exploring the world.",
-    },
+    const WanderlustWizMembers = [
+        {
+            name: "Marcia P Fritzen",
+            position: "CEO",
+            bio: "Hello, I'm Marcia Patricia Fritzen, a dedicated Code Institute student and the founder of Wanderlust Wiz Quiz. I'm on a journey to combine my passion for coding with my love for exploring the world.",
+        },
 
-    {
-        name: "Julia S Roberts",
-        position: "COO",
-        bio: "Fantasm staff. Meet Julia Roberts, a dynamic and visionary leader in the corporate world. As the Chief Operating Officer (COO) of our esteemed organization, Julia brings a wealth of experience, innovation, and a touch of charisma to our executive team.",
-    },
+        {
+            name: "Julia S Roberts",
+            position: "COO",
+            bio: "Fantasm staff. Meet Julia Roberts, a dynamic and visionary leader in the corporate world. As the Chief Operating Officer (COO) of our esteemed organization, Julia brings a wealth of experience, innovation, and a touch of charisma to our executive team.",
+        },
 
-    {
-        name: "Leonard de Caprio",
-        position: "CFO",
-        bio: "Meet Leonardo de Caprio the financial maestro and strategic wizard behind our organization's fiscal success. With a career rooted in numbers and a vision that transcends spreadsheets, John is the driving force behind our financial prosperit",
-    },
-];
-
-
-function aboutUs() {
-    const aboutUs = document.getElementById("aboutus-container");
-
-    const heading = document.createElement("h1");
-    heading.textContent = "About Us";
-    aboutUs.appendChild(heading);
-
-    const teamSection = document.createElement("section");
-    teamSection.innerHTML = "<h2>Wanderlust Wiz team</h2>";
-    aboutUs.appendChild(teamSection);
+        {
+            name: "Leonard de Caprio",
+            position: "CFO",
+            bio: "Meet Leonardo de Caprio the financial maestro and strategic wizard behind our organization's fiscal success. With a career rooted in numbers and a vision that transcends spreadsheets, John is the driving force behind our financial prosperit",
+        },
+    ];
 
 
-    WanderlustWizMembers.forEach(WWmember => {
-        const WWmemberProfile = document.createElement("div");
-        WWmemberProfile.classList.add("WanderlustWizMembers");
+    function aboutUs() {
+        const aboutUs = document.getElementById("aboutus-container");
 
-        const WWmemberName = document.createElement("h3");
-        WWmemberName.textContent = WWmember.name;
+        const heading = document.createElement("h1");
+        heading.textContent = "About Us";
+        aboutUs.appendChild(heading);
 
-        const WWmemberPosition = document.createElement("p");
-        WWmemberPosition.textContent = `Position: ${WWmember.position}`;
+        const teamSection = document.createElement("section");
+        teamSection.innerHTML = "<h2>Wanderlust Wiz team</h2>";
+        aboutUs.appendChild(teamSection);
 
-        const WWmemberBio = document.createElement("p");
-        WWmemberBio.textContent = WWmember.bio;
 
-        WWmemberProfile.appendChild(WWmemberName);
-        WWmemberProfile.appendChild(WWmemberPosition);
-        WWmemberProfile.appendChild(WWmemberBio);
-        teamSection.appendChild(WWmemberProfile);
-    });
-}
+        WanderlustWizMembers.forEach(WWmember => {
+            const WWmemberProfile = document.createElement("div");
+            WWmemberProfile.classList.add("WanderlustWizMembers");
 
-aboutUs();
+            const WWmemberName = document.createElement("h3");
+            WWmemberName.textContent = WWmember.name;
+
+            const WWmemberPosition = document.createElement("p");
+            WWmemberPosition.textContent = `Position: ${WWmember.position}`;
+
+            const WWmemberBio = document.createElement("p");
+            WWmemberBio.textContent = WWmember.bio;
+
+            WWmemberProfile.appendChild(WWmemberName);
+            WWmemberProfile.appendChild(WWmemberPosition);
+            WWmemberProfile.appendChild(WWmemberBio);
+            teamSection.appendChild(WWmemberProfile);
+        });
+    }
+
+    aboutUs();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,245 +1,245 @@
-const $startQuizButton = document.querySelector(".buttonplay");
-const $questionsArea = document.querySelector(".questions-area");
-const $quizRules = document.querySelector(".quiz_rules");
-const $nextQuestion = document.querySelector(".nextQuestion");
-const $answersArea = document.querySelector(".answers-area");
-const $questionQuiz = document.querySelector(".question");
-const questionDisplay = document.getElementById('question');
-const countdownDisplay = document.getElementById('countdown');
+    const $startQuizButton = document.querySelector(".buttonplay");
+    const $questionsArea = document.querySelector(".questions-area");
+    const $quizRules = document.querySelector(".quiz_rules");
+    const $nextQuestion = document.querySelector(".nextQuestion");
+    const $answersArea = document.querySelector(".answers-area");
+    const $questionQuiz = document.querySelector(".question");
+    const questionDisplay = document.getElementById('question');
+    const countdownDisplay = document.getElementById('countdown');
 
 
-let countdownTime = 10; 
-let countdownInterval;
+    let countdownTime = 10; 
+    let countdownInterval;
 
-$startQuizButton.addEventListener("click", startQuiz);
-$nextQuestion.addEventListener("click", displaynextQuestion);
+    $startQuizButton.addEventListener("click", startQuiz);
+    $nextQuestion.addEventListener("click", displaynextQuestion);
 
-let currentQuestionIndex = 0;
-let totalCorrect = 0
+    let currentQuestionIndex = 0;
+    let totalCorrect = 0
 
-function startQuiz() {
-    $quizRules.classList.add("hide");
-    $startQuizButton.classList.add("hide");
-    $questionsArea.classList.remove("hide");
-    $nextQuestion.classList.add("hide");
+    function startQuiz() {
+        $quizRules.classList.add("hide");
+        $startQuizButton.classList.add("hide");
+        $questionsArea.classList.remove("hide");
+        $nextQuestion.classList.add("hide");
 
-    displaynextQuestion();
-}
-
-
-function displaynextQuestion() {
-resetState()
-
-if (questions.length === currentQuestionIndex) {
-return finishQuiz();
-}
-
-$questionQuiz.textContent = questions[currentQuestionIndex].question;
-questions[currentQuestionIndex].answers.forEach(answer => {
-    const newAnswerButton = document.createElement("answer")
-    newAnswerButton.classList.add("buttona", "answer")
-    newAnswerButton.textContent = answer.text
-    if (answer.correct) {
-        newAnswerButton.dataset.correct = answer.correct
+        displaynextQuestion();
     }
-    $answersArea.appendChild(newAnswerButton);
 
-    newAnswerButton.addEventListener("click", pselectAnswer)
-})
-}
 
-function resetState() {
-    while($answersArea.firstChild) {
-        $answersArea.removeChild($answersArea.firstChild);
+    function displaynextQuestion() {
+    resetState()
+
+    if (questions.length === currentQuestionIndex) {
+    return finishQuiz();
     }
-document.body.removeAttribute("class")
-$nextQuestion.classList.add("hide")
-}
 
-function pselectAnswer(event) {
-const answerClickedByPlayer = event.target
+    $questionQuiz.textContent = questions[currentQuestionIndex].question;
+    questions[currentQuestionIndex].answers.forEach(answer => {
+        const newAnswerButton = document.createElement("answer")
+        newAnswerButton.classList.add("buttona", "answer")
+        newAnswerButton.textContent = answer.text
+        if (answer.correct) {
+            newAnswerButton.dataset.correct = answer.correct
+        }
+        $answersArea.appendChild(newAnswerButton);
 
-if (answerClickedByPlayer.dataset.correct) {
-    document.body.classList.add("correct")
-    incrementScore();
-    totalCorrect++
-  } else {
-    document.body.classList.add("incorrect") 
-    incrementWrongAnswer();
-  }
-
-document.querySelectorAll(".answer").forEach(button => {
-    button.disabled = true
-
-    if (button.dataset.correct) {
-        button.classList.add("correct")
-      } else {
-        button.classList.add("incorrect")
-          }
+        newAnswerButton.addEventListener("click", pselectAnswer)
     })
-    
-    $nextQuestion.classList.remove("hide")
-    currentQuestionIndex++
-  }
-
-  function finishQuiz() {
-    const totalQuestionsGame = questions.length
-    const PlayerPeformance = Math.floor (totalCorrect * 100 / totalQuestionsGame)
-
-    let message = ""
-
-      switch (true) {
-        case(PlayerPeformance >= 90 ):
-        message = "Great Performance :O)"
-        break
-        case (PlayerPeformance >= 70):
-        message = "Good Performance ;)"
-        break
-        case(PlayerPeformance >= 50):
-        message = "Only Good"
-        break
-        default:
-        message = "Poor performance :("
     }
 
+    function resetState() {
+        while($answersArea.firstChild) {
+            $answersArea.removeChild($answersArea.firstChild);
+        }
+    document.body.removeAttribute("class")
+    $nextQuestion.classList.add("hide")
+    }
 
-    $questionsArea.innerHTML =
-    `
-    <p class ="QuizFinal">
-      You got ${totalCorrect} responses correct of ${totalQuestionsGame} questions!
-      <span>Result: ${message}</span>
-    </p>
-    <button class ="answer buttona"
-      onclick=window.location.reload() 
-      class="button">
-      Try again
-    </button>
-  `
-}
+    function pselectAnswer(event) {
+    const answerClickedByPlayer = event.target
 
-/* get the currect score */
-
-function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-}
-
-/* get the incorrect score */
-
-function incrementWrongAnswer() {
-    let oldScore = parseInt(document.getElementById("wrong").innerText);
-    document.getElementById("wrong").innerText =  ++oldScore;
-}
-
-/* countdown */
-
-function startCountdown() {
-    clearInterval(countdownInterval); 
-    countdownTime = 60; 
-    updateCountdown(); 
-    countdownInterval = setInterval(updateCountdown, 1000); 
-}
-function updateCountdown() {
-    countdownDisplay.textContent = countdownTime;
-
-    if (countdownTime === 0) {
-        clearInterval(countdownInterval);
-        countdownDisplay.textContent = 'Time\'s up!';
+    if (answerClickedByPlayer.dataset.correct) {
+        document.body.classList.add("correct")
+        incrementScore();
+        totalCorrect++
     } else {
-        countdownTime--;
+        document.body.classList.add("incorrect") 
+        incrementWrongAnswer();
     }
-}
-// Function to move to the next question
-function nextQuestioncount() {
-    clearInterval(countdownInterval); // totally clear the countdown
-    questionDisplay.textContent = `Question ${questionNumber}`;
-    startCountdown(); 
-}
 
-startCountdown();
+    document.querySelectorAll(".answer").forEach(button => {
+        button.disabled = true
+
+        if (button.dataset.correct) {
+            button.classList.add("correct")
+        } else {
+            button.classList.add("incorrect")
+            }
+        })
+        
+        $nextQuestion.classList.remove("hide")
+        currentQuestionIndex++
+    }
+
+    function finishQuiz() {
+        const totalQuestionsGame = questions.length
+        const PlayerPeformance = Math.floor (totalCorrect * 100 / totalQuestionsGame)
+
+        let message = ""
+
+        switch (true) {
+            case(PlayerPeformance >= 90 ):
+            message = "Great Performance :O)"
+            break
+            case (PlayerPeformance >= 70):
+            message = "Good Performance ;)"
+            break
+            case(PlayerPeformance >= 50):
+            message = "Only Good"
+            break
+            default:
+            message = "Poor performance :("
+        }
+
+
+        $questionsArea.innerHTML =
+        `
+        <p class ="QuizFinal">
+        You got ${totalCorrect} responses correct of ${totalQuestionsGame} questions!
+        <span>Result: ${message}</span>
+        </p>
+        <button class ="answer buttona"
+        onclick=window.location.reload() 
+        class="button">
+        Try again
+        </button>
+    `
+    }
+
+    /* get the currect score */
+
+    function incrementScore() {
+        let oldScore = parseInt(document.getElementById("score").innerText);
+        document.getElementById("score").innerText = ++oldScore;
+    }
+
+    /* get the incorrect score */
+
+    function incrementWrongAnswer() {
+        let oldScore = parseInt(document.getElementById("wrong").innerText);
+        document.getElementById("wrong").innerText =  ++oldScore;
+    }
+
+    /* countdown */
+
+    function startCountdown() {
+        clearInterval(countdownInterval); 
+        countdownTime = 60; 
+        updateCountdown(); 
+        countdownInterval = setInterval(updateCountdown, 1000); 
+    }
+    function updateCountdown() {
+        countdownDisplay.textContent = countdownTime;
+
+        if (countdownTime === 0) {
+            clearInterval(countdownInterval);
+            countdownDisplay.textContent = 'Time\'s up!';
+        } else {
+            countdownTime--;
+        }
+    }
+    // Function to move to the next question
+    function nextQuestioncount() {
+        clearInterval(countdownInterval); // totally clear the countdown
+        questionDisplay.textContent = `Question ${questionNumber}`;
+        startCountdown(); 
+    }
+
+    startCountdown();
 
 
 
-const questions = [
-    {
-        question: "What is the largest country in the world by land area?",
-        answers: [
-            { text: "China", correct: false },
-            { text: "Russia", correct: true },
-            { text: "United States", correct: false },
-            { text: "Germany", correct: false },
-        ]
-    },
-    {
-        question: "Which European city is known as the City of Love and City of Lights?",
-        answers: [
-            { text: "Milan, Italy", correct: false },
-            { text: "Berlin, Germany", correct: false },
-            { text: "Eindhoven, Netherlands", correct: false },
-            { text: "Paris, France", correct: true },
-        ]
-    },
-    {
-        question: "Which country is famous for its ancient city of Petra, carved into rose-red cliffs?",
-        answers: [
-            { text: "Jordan", correct: true },
-            { text: "Greece", correct: false },
-            { text: "Egypt", correct: false },
-            { text: "Germany", correct: false },
-        ]
-    },
-    {
-        question: "In which country can you find the Great Barrier Reef, the world's largest coral reef system?",
-        answers: [
-            { text: "Fiji", correct: false },
-            { text: "Thailand", correct: false },
-            { text: "Australia", correct: true },
-            { text: "Germany", correct: false },
-        ]
-    },
-    {
-        question: "Which African country is known as the Land of a Thousand Hills?",
-        answers: [
-            { text: "Rwanda", correct: true },
-            { text: "Namibia", correct: false },
-            { text: "Tanzania", correct: false },
-            { text: "Algeria", correct: false },
-        ]
-    },
-    {
-        question: "What is the tallest mountain in the world?",
-        answers: [
-            { text: "Manaslu", correct: false },
-            { text: "Kangchenjunga", correct: false },
-            { text: "Makalu", correct: false },
-            { text: "Mount Everest", correct: true },
-        ]
-    },
-    {
-        question: "Which city is home to the famous Christ the Redeemer statue?",
-        answers: [
-            { text: "Rio de Janeiro, Brazil", correct: true },
-            { text: "Lisbon, Portugal", correct: false },
-            { text: "Rome, Italy", correct: false },
-            { text: "Philippines", correct: false },
-        ]
-    },
-    {
-        question: "What is the official language of Brazil?",
-        answers: [
-            { text: "Portuguese", correct: true },
-            { text: "Spanish", correct: false },
-            { text: "Italian", correct: false },
-            { text: "English", correct: false },
-        ]
-    },
-    {
-        question: "In which country would you find the historic site of Machu Picchu?",
-        answers: [
-            { text: "Brazil", correct: false },
-            { text: "Peru", correct: true },
-            { text: "Argentina", correct: false },
-            { text: "Chile", correct: false },
-        ]
-    },
-];
+    const questions = [
+        {
+            question: "What is the largest country in the world by land area?",
+            answers: [
+                { text: "China", correct: false },
+                { text: "Russia", correct: true },
+                { text: "United States", correct: false },
+                { text: "Germany", correct: false },
+            ]
+        },
+        {
+            question: "Which European city is known as the City of Love and City of Lights?",
+            answers: [
+                { text: "Milan, Italy", correct: false },
+                { text: "Berlin, Germany", correct: false },
+                { text: "Eindhoven, Netherlands", correct: false },
+                { text: "Paris, France", correct: true },
+            ]
+        },
+        {
+            question: "Which country is famous for its ancient city of Petra, carved into rose-red cliffs?",
+            answers: [
+                { text: "Jordan", correct: true },
+                { text: "Greece", correct: false },
+                { text: "Egypt", correct: false },
+                { text: "Germany", correct: false },
+            ]
+        },
+        {
+            question: "In which country can you find the Great Barrier Reef, the world's largest coral reef system?",
+            answers: [
+                { text: "Fiji", correct: false },
+                { text: "Thailand", correct: false },
+                { text: "Australia", correct: true },
+                { text: "Germany", correct: false },
+            ]
+        },
+        {
+            question: "Which African country is known as the Land of a Thousand Hills?",
+            answers: [
+                { text: "Rwanda", correct: true },
+                { text: "Namibia", correct: false },
+                { text: "Tanzania", correct: false },
+                { text: "Algeria", correct: false },
+            ]
+        },
+        {
+            question: "What is the tallest mountain in the world?",
+            answers: [
+                { text: "Manaslu", correct: false },
+                { text: "Kangchenjunga", correct: false },
+                { text: "Makalu", correct: false },
+                { text: "Mount Everest", correct: true },
+            ]
+        },
+        {
+            question: "Which city is home to the famous Christ the Redeemer statue?",
+            answers: [
+                { text: "Rio de Janeiro, Brazil", correct: true },
+                { text: "Lisbon, Portugal", correct: false },
+                { text: "Rome, Italy", correct: false },
+                { text: "Philippines", correct: false },
+            ]
+        },
+        {
+            question: "What is the official language of Brazil?",
+            answers: [
+                { text: "Portuguese", correct: true },
+                { text: "Spanish", correct: false },
+                { text: "Italian", correct: false },
+                { text: "English", correct: false },
+            ]
+        },
+        {
+            question: "In which country would you find the historic site of Machu Picchu?",
+            answers: [
+                { text: "Brazil", correct: false },
+                { text: "Peru", correct: true },
+                { text: "Argentina", correct: false },
+                { text: "Chile", correct: false },
+            ]
+        },
+    ];
