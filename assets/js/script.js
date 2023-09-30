835c6a8f0605c1bc3d5f1787150269c5d291dad6
mpaty24 <>
2023-09-30 18:46:40 +0000

    Add more questions to the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,143 @@
+    
+     const questions = [
+        {
+            question: "What is the largest country in the world by land area?",
+            answers: [
+                { text: "China", correct: false },
+                { text: "Russia", correct: true },
+                { text: "United States", correct: false },
+                { text: "Germany", correct: false },
+            ]
+        },
+        {
+            question: "Which European city is known as the City of Love and City of Lights?",
+            answers: [
+                { text: "Milan, Italy", correct: false },
+                { text: "Berlin, Germany", correct: false },
+                { text: "Eindhoven, Netherlands", correct: false },
+                { text: "Paris, France", correct: true },
+            ]
+        },
+        {
+            question: "Which country is famous for its ancient city of Petra, carved into rose-red cliffs?",
+            answers: [
+                { text: "Jordan", correct: true },
+                { text: "Greece", correct: false },
+                { text: "Egypt", correct: false },
+                { text: "Germany", correct: false },
+            ]
+        },
+        {
+            question: "In which country can you find the Great Barrier Reef, the world's largest coral reef system?",
+            answers: [
+                { text: "Fiji", correct: false },
+                { text: "Thailand", correct: false },
+                { text: "Australia", correct: true },
+                { text: "Germany", correct: false },
+            ]
+        },
+        {
+            question: "Which African country is known as the Land of a Thousand Hills?",
+            answers: [
+                { text: "Rwanda", correct: true },
+                { text: "Namibia", correct: false },
+                { text: "Tanzania", correct: false },
+                { text: "Algeria", correct: false },
+            ]
+        },
+        {
+            question: "What is the tallest mountain in the world?",
+            answers: [
+                { text: "Manaslu", correct: false },
+                { text: "Kangchenjunga", correct: false },
+                { text: "Makalu", correct: false },
+                { text: "Mount Everest", correct: true },
+            ]
+        },
+        {
+            question: "Which city is home to the famous Christ the Redeemer statue?",
+            answers: [
+                { text: "Rio de Janeiro, Brazil", correct: true },
+                { text: "Lisbon, Portugal", correct: false },
+                { text: "Rome, Italy", correct: false },
+                { text: "Philippines", correct: false },
+            ]
+        },
+        {
+            question: "What is the official language of Brazil?",
+            answers: [
+                { text: "Portuguese", correct: true },
+                { text: "Spanish", correct: false },
+                { text: "Italian", correct: false },
+                { text: "English", correct: false },
+            ]
+        },
+        {
+            question: "In which country would you find the historic site of Machu Picchu?",
+            answers: [
+                { text: "Brazil", correct: false },
+                { text: "Peru", correct: true },
+                { text: "Argentina", correct: false },
+                { text: "Chile", correct: false },
+            ]
+        },
+        {
+            question: "What is the largest desert in the world?",
+            answers: [
+                { text: "The Sahara Desert", correct: true },
+                { text: "Arabian Desert", correct: false },
+                { text: "Gobi Desert", correct: false },
+                { text: "Rubʿal-Khali Desert", correct: false },
+            ]
+        },
+        {
+            question: "Which European city is divided into two parts by the Bosphorus Strait?",
+            answers: [
+                { text: "Edirne", correct: false },
+                { text: "Eskişehir", correct: false },
+                { text: "Istanbul", correct: true },
+                { text: "Bolu", correct: false },
+            ]
+        },
+        {
+            question: "Which Asian city is often referred to as the Lion City?",
+            answers: [
+                { text: "Hanoi", correct: false },
+                { text: "Hong Kong", correct: false },
+                { text: "Bangkok", correct: false },
+                { text: "Singapore", correct: true },
+            ]
+        },
+        {
+            question: "What is the official language of Argentina?",
+            answers: [
+                { text: "Spanish", correct: true },
+                { text: "Guaraní", correct: false },
+                { text: "English", correct: false },
+                { text: "Portuguese", correct: false },
+            ]
+        },
+        {
+            question: "In which U.S. state can you find the Grand Canyon?",
+            answers: [
+                { text: "California", correct: false },
+                { text: "Texas", correct: false },
+                { text: "Arizona", correct: true },
+                { text: "Florida", correct: false },
+            ]
+        },
+        {
+            question: "What is the largest island in the Mediterranean Sea?",
+            answers: [
+                { text: "Sicily, Italy", correct: true },
+                { text: "Cyprus, Cyprus", correct: false },
+                { text: "Mykonos, Greece", correct: false },
+                { text: "Ibiza, Spain", correct: false },
+            ]
+        },
+    ];
+    
+    
     const $startQuizButton = document.querySelector(".buttonplay");
     const $questionsArea = document.querySelector(".questions-area");
     const $quizRules = document.querySelector(".quiz_rules");
@@ -160,88 +300,3 @@
 
     startCountdown();
 
-
-
-    const questions = [
-        {
-            question: "What is the largest country in the world by land area?",
-            answers: [
-                { text: "China", correct: false },
-                { text: "Russia", correct: true },
-                { text: "United States", correct: false },
-                { text: "Germany", correct: false },
-            ]
-        },
-        {
-            question: "Which European city is known as the City of Love and City of Lights?",
-            answers: [
-                { text: "Milan, Italy", correct: false },
-                { text: "Berlin, Germany", correct: false },
-                { text: "Eindhoven, Netherlands", correct: false },
-                { text: "Paris, France", correct: true },
-            ]
-        },
-        {
-            question: "Which country is famous for its ancient city of Petra, carved into rose-red cliffs?",
-            answers: [
-                { text: "Jordan", correct: true },
-                { text: "Greece", correct: false },
-                { text: "Egypt", correct: false },
-                { text: "Germany", correct: false },
-            ]
-        },
-        {
-            question: "In which country can you find the Great Barrier Reef, the world's largest coral reef system?",
-            answers: [
-                { text: "Fiji", correct: false },
-                { text: "Thailand", correct: false },
-                { text: "Australia", correct: true },
-                { text: "Germany", correct: false },
-            ]
-        },
-        {
-            question: "Which African country is known as the Land of a Thousand Hills?",
-            answers: [
-                { text: "Rwanda", correct: true },
-                { text: "Namibia", correct: false },
-                { text: "Tanzania", correct: false },
-                { text: "Algeria", correct: false },
-            ]
-        },
-        {
-            question: "What is the tallest mountain in the world?",
-            answers: [
-                { text: "Manaslu", correct: false },
-                { text: "Kangchenjunga", correct: false },
-                { text: "Makalu", correct: false },
-                { text: "Mount Everest", correct: true },
-            ]
-        },
-        {
-            question: "Which city is home to the famous Christ the Redeemer statue?",
-            answers: [
-                { text: "Rio de Janeiro, Brazil", correct: true },
-                { text: "Lisbon, Portugal", correct: false },
-                { text: "Rome, Italy", correct: false },
-                { text: "Philippines", correct: false },
-            ]
-        },
-        {
-            question: "What is the official language of Brazil?",
-            answers: [
-                { text: "Portuguese", correct: true },
-                { text: "Spanish", correct: false },
-                { text: "Italian", correct: false },
-                { text: "English", correct: false },
-            ]
-        },
-        {
-            question: "In which country would you find the historic site of Machu Picchu?",
-            answers: [
-                { text: "Brazil", correct: false },
-                { text: "Peru", correct: true },
-                { text: "Argentina", correct: false },
-                { text: "Chile", correct: false },
-            ]
-        },
-    ];
